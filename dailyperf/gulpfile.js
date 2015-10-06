3351f7b1ee2e9a3c986e0d3fdfc29ecc57fcff7a
Fabien Rogeret <fabien.rogeret@gmail.com>
2015-10-05 23:49:16 -0400

    Base structure & gulp conf

--- dailyperf/gulpfile.js
+++ dailyperf/gulpfile.js
@@ -1,28 +1,27 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+
 
 gulp.task('sass', function () {
+    var sass = require('gulp-sass');
+
     gulp.src('css/src/style.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('./css'));
 });
 
-var babel = require("gulp-babel"),
-    sourcemaps = require("gulp-sourcemaps"),
-    concat = require("gulp-concat");
+gulp.task('build', function () {
+    var concat = require('gulp-concat'),
+        uglify = require('gulp-uglify');
 
-gulp.task("babel", function () {
-    return gulp.src(["scripts/src/card-loader.js", "scripts/src/app.js"])
-        .pipe(sourcemaps.init())
-        .pipe(babel())
-        .pipe(concat("app.js"))
-        .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest("scripts"));
+    gulp.src(['bower_components/doT/doT.js'])
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('scripts/'));
+
 });
 
 gulp.task('dailyperf', function () {
     gulp.watch('./css/src/*.scss', ['sass']);
-    gulp.watch('./scripts/src/*.js', ['babel']);
+    gulp.watch('./scripts/src/*.js', ['build']);
 });
