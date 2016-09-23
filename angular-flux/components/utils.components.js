a26ca029181f0a56af5b941f90dc0d91712ae1e7
Fabien Rogeret <fabien.rogeret@gmail.com>
2016-09-22 21:11:13 -0400

    Clean up

--- angular-flux/components/utils.components.js
+++
@@ -1,7 +0,0 @@
-angular
-    .module('components')
-	.service('dateUtils', function () {
-		this.getDateLabel = (date) => {
-			return date.getMonth() + "/" + date.getDate();
-		}
-	})
--- pomodoro/app/reducers/reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import tasks from './tasks';
-
-const pomodoroApp = combineReducers({
-	tasks
-})
-
-export default pomodoroApp;
